a061bd8185823851445c40813bd87818c9ac0700
texasroh <texasroh@gmail.com>
2022-07-30 12:30:46 -0400

    install react-router-dom

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Routes } from "react-router-dom";
 import styles from "./app.module.css";
 import Login from "./components/login/login";
 
 function App({ authService }) {
-    return (
-        <div className={styles.app}>
-            <Login authService={authService} />
-        </div>
-    );
+  return (
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+      <Login authService={authService} />
+    </div>
+  );
 }
 
 export default App;
