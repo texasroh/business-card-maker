70f739fc041c6986d95a2886eff7bd965036a146
texasroh <texasroh@gmail.com>
2022-08-10 00:19:25 -0400

    style card preview

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./maker.module.css";
 import Header from "../header/header";
 import Footer from "../footer/footer";
@@ -7,6 +7,41 @@ import Editor from "../editor/editor";
 import Preview from "../preview/preview";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "Ellie",
+      company: "Samsung",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "Ellie",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: null,
+    },
+    {
+      id: "3",
+      name: "Ellie",
+      company: "Samsung",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: null,
+    },
+  ]);
   const navigate = useNavigate();
   const onLogout = () => {
     authService.logout();
@@ -24,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
