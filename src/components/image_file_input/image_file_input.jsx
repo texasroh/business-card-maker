b10288ad838fecb7a192d8466b05c822ca56195e
texasroh <texasroh@gmail.com>
2022-08-15 03:16:01 -0400

    misc

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./image_file_input.module.css";
 
-const ImageFileInput = ({ imageUploader }) => <button>Image</button>;
+const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const inputRef = useRef();
+  const onButtonClick = () => {
+    inputRef.current.click();
+  };
+  return (
+    <div className={styles.container}>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        type="file"
+        accept="image/*"
+        name="file"
+      />
+      <button className={styles.button} onClick={onButtonClick}>
+        {name || "No file"}
+      </button>
+    </div>
+  );
+};
 
 export default ImageFileInput;
