e90e3525a25a9a3767a6964a36dd79132a432eda
June Roh <june.roh@streetsmarket.com>
2022-08-15 10:16:02 -0400

    handle image uploading

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -2,24 +2,34 @@ import React, { useRef } from "react";
 import styles from "./image_file_input.module.css";
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
-  const inputRef = useRef();
-  const onButtonClick = () => {
-    inputRef.current.click();
-  };
-  return (
-    <div className={styles.container}>
-      <input
-        ref={inputRef}
-        className={styles.input}
-        type="file"
-        accept="image/*"
-        name="file"
-      />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || "No file"}
-      </button>
-    </div>
-  );
+    const inputRef = useRef();
+    const onButtonClick = (event) => {
+        event.preventDefault();
+        inputRef.current.click();
+    };
+    const onChange = async (event) => {
+        event.preventDefault();
+        const uploaded = await imageUploader.upload(event.target.files[0]);
+        onFileChange({
+            name: uploaded.original_filename,
+            url: uploaded.url,
+        });
+    };
+    return (
+        <div className={styles.container}>
+            <input
+                ref={inputRef}
+                className={styles.input}
+                type="file"
+                accept="image/*"
+                name="file"
+                onChange={onChange}
+            />
+            <button className={styles.button} onClick={onButtonClick}>
+                {name || "No file"}
+            </button>
+        </div>
+    );
 };
 
 export default ImageFileInput;
--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -1,7 +1,16 @@
 class ImageUploader {
-  upload(file) {
-    return "file";
-  }
+    async upload(file) {
+        const url = "https://api.cloudinary.com/v1_1/dvwft02io/image/upload";
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("upload_preset", "fb6veduf");
+
+        const res = await fetch(url, {
+            method: "POST",
+            body: formData,
+        });
+        return await res.json();
+    }
 }
 
 export default ImageUploader;
