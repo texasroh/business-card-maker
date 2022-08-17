a96451de5e4bf61cf4749e6504004db62c7e572a
texasroh <texasroh@gmail.com>
2022-08-17 02:44:26 -0400

    use firebase frealtime database - sync cards

--- src/service/card_repository.js
+++ src/service/card_repository.js
@@ -2,21 +2,28 @@ import { getDatabase, ref, set, remove, onValue } from "firebase/database";
 import { firebaseApp } from "./firebase";
 
 class CardRepository {
-    constructor() {
-        this.db = getDatabase(firebaseApp);
-    }
+  constructor() {
+    this.db = getDatabase(firebaseApp);
+  }
 
-    syncCard(userId, onUpdate) {
-        const ref = ref(this.db, `${userId}/cards`);
-        onValue(ref, (snapshot) => {}, { onlyOnce: true });
-    }
+  syncCards(userId, onUpdate) {
+    const sync_ref = ref(this.db, `${userId}/cards`);
+    onValue(
+      sync_ref,
+      (snapshot) => {
+        const value = snapshot.val();
+        value && onUpdate(value);
+      },
+      { onlyOnce: true }
+    );
+  }
 
-    saveCard(userId, card) {
-        set(ref(this.db, `${userId}/cards/${card.id}`), card);
-    }
-    removeCard(userId, card) {
-        remove(ref(this.db, `${userId}/cards/${card.id}`));
-    }
+  saveCard(userId, card) {
+    set(ref(this.db, `${userId}/cards/${card.id}`), card);
+  }
+  removeCard(userId, card) {
+    remove(ref(this.db, `${userId}/cards/${card.id}`));
+  }
 }
 
 export default CardRepository;
